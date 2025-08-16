72d495577ff71766278fa40ef9acdc89157ab695
SeiyaSugiokaPi <seiya_sugioka@post.pioneer.co.jp>
2025-08-16 17:04:22 +0900

    ✨ミニアプリ作成(Demoアプリ作成)

---
+++ AppFeatureDemo/AppFeatureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFeatureDemoApp.swift
+//  AppFeatureDemo
+//
+//  Created by USER on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct AppFeatureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppFeatureDemo/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  AppFeatureDemo
+//
+//  Created by USER on 2025/08/16.
+//
+
+import SwiftUI
+import AppFeature
+
+struct ContentView: View {
+    var body: some View {
+        CounterScreen()
+    }
+}
+
+#Preview {
+    ContentView()
+}
